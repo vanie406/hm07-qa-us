ebe6d94fe85de768b5fca923561a077b0095e4c5
VanVan <vanessadimacuha2@gmail.com>
2024-09-02 10:15:51 +0300

    trying to push my commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL:    " https://cnt-cb1ad94a-16c3-4f74-a468-44967f31284e.containerhub.tripleten-services.com"
+    API_URL:    " https://cnt-10afeed2-9c81-479d-800a-68eb8f9c4381.containerhub.tripleten-services.com"
 }
--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -86,6 +86,6 @@ test('shoud update the kit name', async () => {
 		
 
     } catch (error) {
-        console.error(error);
+        console.error(error);gitcd
     }
 });
