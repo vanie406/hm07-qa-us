1057d1e2c21b6c73570167bab7f96edf1ca481eb
VanVan <vanessadimacuha2@gmail.com>
2024-09-02 13:37:53 +0300

    try to commit

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-undef
 const config = require('../config');
 
 test('status code should be 200', async () => {
@@ -6,7 +5,7 @@ test('status code should be 200', async () => {
   let actualStatus;
 
   try {
-      response = await fetch(`${config.API_URL}/api/v1/warehouses`, {
+    response = await fetch(`${config.API_URL}/api/v1/warehouses`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -22,34 +21,32 @@ test('status code should be 200', async () => {
   expect(actualStatus).toBe(200);
 });
 
-
-
 test('working hours in Big World should be correct', async () => {
   let response;
   let responseData;
   let bigWorldStore;
 
   try {
-      response = await fetch(`${config.API_URL}/api/v1/warehouses`, {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json'
-      }
+    response = await fetch(`${config.API_URL}/api/v1/warehouses`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
 
-     responseData = await response.json();
+    responseData = await response.json();
 
-     bigWorldStore = responseData.find(store => store.name === "Big World");
+    bigWorldStore = responseData.find(store => store.name === "Big World");
 
     const expectedWorkingHours = {
       start: 5,
-      end: 20
+      end: 20,
     };
-
-  } catch (error) {
-    console.error(error);
 
     expect(bigWorldStore.workingHours.start).toBe(expectedWorkingHours.start);
     expect(bigWorldStore.workingHours.end).toBe(expectedWorkingHours.end);
+
+  } catch (error) {
+    console.error(error);
   }
 });
