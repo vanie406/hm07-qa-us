9489772585d072db8bed459dad38291fd891b2ee
VanVan <vanessadimacuha2@gmail.com>
2024-09-02 14:03:39 +0300

    try to commit

--- tests/postHandlers.test.js
+++ tests/postHandlers.test.js
@@ -2,8 +2,7 @@
 const config = require('../config');
 
 const requestBody = {
-
-		"productsList": [
+    "productsList": [
         {
             "id": 1,
             "quantity": 2
@@ -16,51 +15,50 @@ const requestBody = {
 }
 
 test('status should response 200', async () => {
-	let response;
-	let actualStatus;
+    let response;
+    let actualStatus;
 
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/6/products`, {
-			method: 'POST',
-			headers: {
-			'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(requestBody)
-		});
+        response = await fetch(`${config.API_URL}/api/v1/kits/6/products`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        });
 
-		actualStatus = response.status;
-		console.log('Received status:', actualStatus); 
+        actualStatus = response.status;
+        console.log('Received status:', actualStatus); 
 
-	} catch (error) {
-		console.error(error);
-	}
+    } catch (error) {
+        console.error(error);
+    }
 
-	expect(actualStatus).toBe(200);
+    expect(actualStatus).toBe(200);
 });
 
-
 test('kits should contain productList', async () => {
-	let response;
-	let kitData;
-  
-	try {
-	    response = await fetch(`${config.API_URL}/api/v1/kits/6/products`, {
-		method: 'POST',
-		headers: {
-		  'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(requestBody)
-	  });
-  
-	  if (response.ok) {
-		kitData = await response.json();
-	  } else {
-		console.error('Response error:', response.status);
-	  }
-	} catch (error) {
-	  console.error('Fetch error:', error);
-	}
+    let response;
+    let kitData;
 
-	expect(kitData).toHaveProperty('productsList');
-	expect(Array.isArray(kitData.productsList)).toBe(true);
-  });
+    try {
+        response = await fetch(`${config.API_URL}/api/v1/kits/6/products`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        });
+
+        if (response.ok) {
+            kitData = await response.json();
+        } else {
+            console.error('Response error:', response.status);
+        }
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+
+    expect(kitData).toHaveProperty('productsList');
+    expect(Array.isArray(kitData.productsList)).toBe(true);
+});
