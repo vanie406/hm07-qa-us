9a97967782603cdf99899b5ab8cf3b0d77269b67
VanVan <vanessadimacuha2@gmail.com>
2024-09-02 14:05:41 +0300

    try to commit

--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-const requestBody= {
-	"name": "My modified kit",
+const requestBody = {
+    "name": "My modified kit",
     "productsList": [
         {
             "id": 1,
@@ -24,31 +24,31 @@ const requestBody= {
 }
 
 test('status should respond with 200', async () => {
-	let response;
-	let actualStatus;
+    let response;
+    let actualStatus;
 
-	try {
-		response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
-			method: 'PUT',
-			headers: {
-			'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(requestBody)
-		});
+    try {
+        response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        });
 
-		actualStatus = response.status;
-		console.log('Received status:', actualStatus); 
+        actualStatus = response.status;
+        console.log('Received status:', actualStatus); 
 
     } catch (error) {
         console.error(error);
     }
 
-	expect(actualStatus).toBe(200); 
+    expect(actualStatus).toBe(200); 
 });
 
 
-let updatedRequestBody= {
-	"name": "Shopping Cart",
+let updatedRequestBody = {
+    "name": "Shopping Cart",
     "productsList": [
         {
             "id": 1,
@@ -68,32 +68,33 @@ let updatedRequestBody= {
         }
     ]
 }
-test('shoud update the kit name', async () => {
-	let getResponse;
-	let kitData;
-	
-	try {
-	  await fetch(`${config.API_URL}/api/v1/kits/2`, {
-		method: 'PUT',
-		headers: {
-		  'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(updatedRequestBody)
-	  });
-	  
-	    getResponse = await fetch(`${config.API_URL}/api/v1/kits/2`, {
-		method: 'GET',
-		headers: {
-		  'Content-Type': 'application/json'
-		}
-	  });
-	
-	    kitData = await getResponse.json();
 
-	} catch (error) {
-	  console.error('Fetch error:', error);
-	}
-	
-	expect(kitData).toBeDefined(); 
-	expect(kitData.name).toBe("Shopping Cart");
-});
+test('should update the kit name', async () => {
+    let getResponse;
+    let kitData;
+    
+    try {
+        await fetch(`${config.API_URL}/api/v1/kits/2`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedRequestBody)
+        });
+        
+        getResponse = await fetch(`${config.API_URL}/api/v1/kits/2`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        kitData = await getResponse.json();
+
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+    
+    expect(kitData).toBeDefined(); 
+    expect(kitData.name).toBe("Shopping Cart");
+}); 
