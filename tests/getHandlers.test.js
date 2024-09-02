6b4cae5350c49396bc4370fbf6b86e3c020dc1ab
VanVan <vanessadimacuha2@gmail.com>
2024-09-02 13:56:16 +0300

    try to commit

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -22,32 +22,3 @@ test('status code should be 200', async () => {
 
   expect(actualStatus).toBe(200);
 });
-
-test('working hours in Big World should be correct', async () => {
-  let responseData;
-  let bigWorldStore;
-
-  try {
-    const response = await fetch(`${config.API_URL}/api/v1/warehouses`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    responseData = await response.json();
-
-    bigWorldStore = responseData.find(store => store.name === "Big World");
-
-    const expectedWorkingHours = {
-      start: 5,
-      end: 20,
-    };
-
-    expect(bigWorldStore.workingHours.start).toBe(expectedWorkingHours.start);
-    expect(bigWorldStore.workingHours.end).toBe(expectedWorkingHours.end);
-
-  } catch (error) {
-    console.error(error);
-  }
-});
